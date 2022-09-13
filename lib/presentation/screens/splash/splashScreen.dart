f4123be4cd122be9a6e0f055d635492a583ac85d
Iamvictorsam <iamvictorsam@gmail.com>
2022-09-13 22:16:09 +0100

    added splash sccreen

--- lib/presentation/screens/splash/splashScreen.dart
+++ lib/presentation/screens/splash/splashScreen.dart
@@ -1,10 +1,27 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:dotidoti_app/presentation/helper/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:page_transition/page_transition.dart';
 
-class SplashScreen extends StatelessWidget {
-const SplashScreen({ Key? key }) : super(key: key);
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return Container();
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splashIconSize: Get.height * 0.5,
+      backgroundColor: kPrimary,
+      splash: Image.asset('assets/logo.png'),
+      nextScreen: const SplashScreen(),
+      splashTransition: SplashTransition.slideTransition,
+      pageTransitionType: PageTransitionType.fade,
+    );
   }
 }
