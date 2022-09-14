18d00a8a07513a1ac76e5b1121a1214049da678c
Iamvictorsam <iamvictorsam@gmail.com>
2022-09-15 00:13:02 +0100

    finished home screen

--- lib/presentation/screens/home/components/body.dart
+++ lib/presentation/screens/home/components/body.dart
@@ -15,57 +15,74 @@ class HomeBody extends StatefulWidget {
 class _HomeBodyState extends State<HomeBody> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: 20.w,
-        ),
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 20.h,
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 20.w,
+      ),
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 20.h,
+          ),
+          const WalletCard(),
+          SizedBox(
+            height: Get.height * 0.15,
+          ),
+          GlowButton(
+            onPressed: () {},
+            color: kPrimary,
+            height: 50.h,
+            width: Get.width,
+            child: Text(
+              'Instant Pickup',
+              style: TextStyle(color: kWhite, fontSize: 18.sp),
             ),
-            const WalletCard(),
-            SizedBox(
-              height: Get.height * 0.15,
-            ),
-            GlowButton(
-              onPressed: () {},
-              color: kPrimary,
-              height: 50.h,
-              width: Get.width,
-              child: Text(
-                'Instant Pickup',
-                style: TextStyle(color: kWhite, fontSize: 18.sp),
+          ),
+          SizedBox(
+            height: 40.h,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Latest Pickups',
+                style: TextStyle(
+                    fontSize: 16.sp,
+                    letterSpacing: 1,
+                    fontWeight: FontWeight.w600),
+              ),
+              TextButton.icon(
+                  icon: Text(
+                    'See All',
+                    style: TextStyle(color: kPrimary, fontSize: 16.sp),
+                  ),
+                  label: const Icon(
+                    Icons.arrow_forward_ios,
+                    color: kPrimary,
+                  ),
+                  onPressed: () {}),
+            ],
+          ),
+          SizedBox(
+            height: 5.h,
+          ),
+          //Todo: make the container scrollable
+          Column(
+            children: List.generate(
+              3,
+              (int i) => ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.green,
+                  radius: 15.r,
+                  child: const Icon(Icons.check),
+                ),
+                contentPadding: EdgeInsets.zero,
+                title: const Text('28th Sept, 2022'),
+                trailing: const Text('09:29 am'),
               ),
             ),
-            SizedBox(
-              height: 40.h,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Latest Pickups',
-                  style: TextStyle(
-                      fontSize: 16.sp,
-                      letterSpacing: 1,
-                      fontWeight: FontWeight.w600),
-                ),
-                TextButton.icon(
-                    icon: Text(
-                      'See All',
-                      style: TextStyle(color: kPrimary, fontSize: 16.sp),
-                    ),
-                    label: const Icon(
-                      Icons.arrow_forward_ios,
-                      color: kPrimary,
-                    ),
-                    onPressed: () {}),
-              ],
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
