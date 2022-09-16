0797bcaf326a61487e0018a1f8e1531292a9061c
Iamvictorsam <iamvictorsam@gmail.com>
2022-09-16 02:51:35 +0100

    finished profile screen

--- lib/presentation/screens/profile/profile.dart
+++ lib/presentation/screens/profile/profile.dart
@@ -10,31 +10,36 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: 30.h),
-              CircleAvatar(
-                  radius: 60.r,
-                  child: Text(
-                    "SV",
-                    style: TextStyle(fontSize: 30.sp),
-                  )),
-              SizedBox(height: 20.h),
-              const ProfileMenu(
-                  text: "Pickup History",
-                  icon: Icons.rotate_90_degrees_ccw_outlined),
-              const ProfileMenu(
-                  text: "Payment History", icon: Icons.call_to_action_rounded),
-              const ProfileMenu(
-                  text: "Active Subscription", icon: Icons.rate_review_rounded),
-              const ProfileMenu(
-                  text: "Notification", icon: Icons.notification_important),
-              const ProfileMenu(text: "Settings", icon: Icons.settings),
-              const ProfileMenu(text: "About", icon: Icons.info),
-              const ProfileMenu(text: "Logout", icon: Icons.logout),
-            ],
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 20.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 30.h),
+                CircleAvatar(
+                    radius: 60.r,
+                    child: Text(
+                      "SV",
+                      style: TextStyle(fontSize: 30.sp),
+                    )),
+                SizedBox(height: 20.h),
+                const ProfileMenu(
+                    text: "Pickup History",
+                    icon: Icons.rotate_90_degrees_ccw_outlined),
+                const ProfileMenu(
+                    text: "Payment History",
+                    icon: Icons.call_to_action_rounded),
+                const ProfileMenu(
+                    text: "Active Subscription",
+                    icon: Icons.rate_review_rounded),
+                const ProfileMenu(
+                    text: "Notification", icon: Icons.notification_important),
+                const ProfileMenu(text: "Settings", icon: Icons.settings),
+                const ProfileMenu(text: "About", icon: Icons.info),
+                const ProfileMenu(text: "Logout", icon: Icons.logout),
+              ],
+            ),
           ),
         ),
       ),
